f511e193907eb942f36bd5e1600fd1c36375196c
norsyst <norsyst@gmail.com>
2013-02-10 19:26:23 -0300

    Primera version

---
+++ MW_service/MW_service.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace MW_service
+{
+    public partial class MW_service : ServiceBase
+    {
+        public MW_service()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
